52584f5a487316271dfe22849cdfb4ec785a16fc
elizamac <elizaf.macovei@gmail.com>
2023-11-20 19:47:48 +0000

    Made theme persist

--- scripts/change-theme.js
+++ scripts/change-theme.js
@@ -2,8 +2,17 @@ function changeTheme() {
     if(document.getElementById("theme_css").getAttribute("href") === "style/colors.css") {
         document.getElementById("theme_css").href = "style/colors-dark.css";
         document.getElementById("theme_icon").className = "fa-regular fa-moon";
+        sessionStorage.setItem("theme", "dark");
     } else {
         document.getElementById("theme_css").href = "style/colors.css";
         document.getElementById("theme_icon").className = "fa-regular fa-sun";
+        sessionStorage.setItem("theme", "light");
     }
 };
+
+function persistTheme() {
+    if(document.getElementById("theme_css").getAttribute("href") === "style/colors.css" && sessionStorage.getItem("theme") === "dark") {
+        document.getElementById("theme_css").href = "style/colors-dark.css";
+        document.getElementById("theme_icon").className = "fa-regular fa-moon";
+    }
+}
